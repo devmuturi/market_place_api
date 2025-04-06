baa6b05ce7ea5f85e7b5063ed66526077b491f8e
devmuturi <muturinjonge09@gmail.com>
2025-04-06 21:44:43 +0300

    Set the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # API defination
+  namespace :api, defaults: { format: :json} do
+    namespace :V1 do
+    end
+  end
   get "up" => "rails/health#show", as: :rails_health_check
 end
