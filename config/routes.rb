5624eddeb9d71e1f8527362c4166eb634d3ccbd0
devmuturi <muturinjonge09@gmail.com>
2025-04-06 22:20:50 +0300

    Adds show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # API defination
-  namespace :api, defaults: { format: :json} do
-    namespace :V1 do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [ :show ]
     end
   end
   get "up" => "rails/health#show", as: :rails_health_check
