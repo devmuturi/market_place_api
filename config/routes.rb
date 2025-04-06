cc06c18fe41c4995f4536e6a5f7f0c4aaf77c877
devmuturi <muturinjonge09@gmail.com>
2025-04-06 22:55:04 +0300

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # API defination
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [ :show ]
+      resources :users, only: %i[ show create ]
     end
   end
   get "up" => "rails/health#show", as: :rails_health_check
