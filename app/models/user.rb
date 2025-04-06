d45a3d7cc453dfbc52f0752d5a9523afdd4be6d0
devmuturi <muturinjonge09@gmail.com>
2025-04-06 22:09:36 +0300

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address"
+  has_secure_password
 end
